47429e1a56c1bd11655e2cf223185f934b664a38
Christen <2940836510@qq.com>
2026-09-21 13:56:56 +0800

    update lab02 student management menu

--- python_lab_class/lab02/main.py
+++ python_lab_class/lab02/main.py
@@ -1,9 +1,13 @@
 from process import *
 import ast
+import json
 
 def main():
 
     students = []
+    sorted_students = []
+    grade_student = {}
+    count=()
 
     with open("student.txt", "r", encoding="utf-8") as f:
         for line in f:
@@ -16,9 +20,34 @@ def main():
     print("请选择你想要进行的操作并输入对应的序号：\n")
     print_choice()
     user_choice = input()
+    choice = ['1','2','3','4','5','6','q']
+    if user_choice not in choice:
+        print("输入的序号不正确，请重新输入")
+        return
+    
     while(user_choice != 'q'):
-        if user_choice == ''
-
+        if user_choice == '1':
+            students = student_entry(students)
+        elif user_choice == '2':
+            search(students)
+        elif user_choice == '3':
+            scores_static(students)
+        elif user_choice == '4':
+            sorted_students = student_sorted(students, reverse=True)
+            with open("sorted_students.json", "w", encoding="utf-8") as f:
+                json.dump(sorted_students, f, ensure_ascii=False, indent = 4)
+                print("排序后的列表已存储到sorted_students.json文件\n")
+        elif user_choice == '5':
+            fail_warning(students)
+        elif user_choice == '6':
+            grade_student, count = grade_classification(students)
+            print("请选择你想要进行的操作并输入对应的序号：\n")
+            print_choice()
+            user_choice = input()
+            choice = ['1','2','3','4','5','6','q']
+            if user_choice not in choice:
+                print("输入的序号不正确，请重新输入")
+                return
 
 
 
